2ff375f4f8b5710e22c10741a333e7b95f261976
Daniel Quackenbush <thequack45@gmail.com>
2015-12-02 22:32:12 -0500

    Original, completed commit

---
+++ CsgoCaseOpenProgram/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace CsgoCaseOpenProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.importXMLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.caseListBox = new System.Windows.Forms.ListBox();
+            this.xmlImportDialog = new System.Windows.Forms.OpenFileDialog();
+            this.rollOutputRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importXMLToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(90, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // importXMLToolStripMenuItem
+            // 
+            this.importXMLToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.importXMLToolStripMenuItem.Name = "importXMLToolStripMenuItem";
+            this.importXMLToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
+            this.importXMLToolStripMenuItem.Text = "Import XML";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(12, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Roll";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // caseListBox
+            // 
+            this.caseListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.caseListBox.FormattingEnabled = true;
+            this.caseListBox.Location = new System.Drawing.Point(12, 70);
+            this.caseListBox.Name = "caseListBox";
+            this.caseListBox.Size = new System.Drawing.Size(260, 95);
+            this.caseListBox.TabIndex = 3;
+            this.caseListBox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // xmlImportDialog
+            // 
+            this.xmlImportDialog.FileName = "xmlImportDialog";
+            this.xmlImportDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // rollOutputRichTextBox
+            // 
+            this.rollOutputRichTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rollOutputRichTextBox.BackColor = System.Drawing.Color.Black;
+            this.rollOutputRichTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F);
+            this.rollOutputRichTextBox.ForeColor = System.Drawing.Color.White;
+            this.rollOutputRichTextBox.Location = new System.Drawing.Point(12, 27);
+            this.rollOutputRichTextBox.Name = "rollOutputRichTextBox";
+            this.rollOutputRichTextBox.ReadOnly = true;
+            this.rollOutputRichTextBox.Size = new System.Drawing.Size(260, 37);
+            this.rollOutputRichTextBox.TabIndex = 4;
+            this.rollOutputRichTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 203);
+            this.Controls.Add(this.rollOutputRichTextBox);
+            this.Controls.Add(this.caseListBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Random Roll Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem importXMLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox caseListBox;
+        private System.Windows.Forms.OpenFileDialog xmlImportDialog;
+        private System.Windows.Forms.RichTextBox rollOutputRichTextBox;
+    }
+}
+
---
+++ CsgoCaseOpenProgram/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace CsgoCaseOpenProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // rollOutputRichTextBox.ForeColor = System.Drawing.Color.Red;
+            // ^ How to change text color
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // ^ "Import XML -> Open" clicked
+            // Display XML import file dialog
+            DialogResult importedXmlResult = xmlImportDialog.ShowDialog();
+            if (importedXmlResult == DialogResult.OK)
+            {
+                XmlDocument configXml = new XmlDocument();
+                configXml.Load(xmlImportDialog.FileName);
+
+                XmlNode configXmlRoot = configXml.FirstChild;
+                XmlNode configXmlCaseList = configXmlRoot["cases"];
+
+                string currentCaseName = null;
+
+                for (int i = 0; i < configXmlCaseList.ChildNodes.Count; i++)
+                {
+                    currentCaseName = configXmlCaseList.ChildNodes[i].Attributes["caseName"].Value;
+                    caseListBox.Items.Add(currentCaseName);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Reset text box color and value
+            rollOutputRichTextBox.Text = "";
+            rollOutputRichTextBox.ForeColor = Color.White;
+
+            // Load XML file
+            XmlDocument configXml = new XmlDocument();
+            configXml.Load(xmlImportDialog.FileName);
+
+            // Select possible guns list from selected case; stored in XmlNodeList `possibleGunsList`
+            XmlNode caseList = configXml.FirstChild["cases"];
+            XmlNode selectedCase = caseList.SelectNodes("case[@caseName='" + caseListBox.SelectedItem.ToString() + "']")[0];
+            XmlNodeList possibleGunsList = selectedCase["possibleGuns"].ChildNodes;
+
+            // Read odds, store in `classOddsDict`
+            XmlNode classOddsXml = configXml.FirstChild["odds"]["classOdds"];
+            int classes = classOddsXml.ChildNodes.Count;
+            OrderedDictionary classOddsDict = new OrderedDictionary();
+            string className = null;
+            double classOdds = 0;
+            for (int i = 0; i < classes; i++)
+            {
+                className = classOddsXml.ChildNodes[i].SelectNodes("className")[0].InnerText;
+                classOdds = Convert.ToDouble(classOddsXml.ChildNodes[i].SelectNodes("classOdds")[0].InnerText);
+                classOddsDict.Add(className, classOdds);
+            }
+
+            // Choose random class, stores color in `selectedColor`
+            Random randomObject = new Random();
+            double randomColorRoll = randomObject.NextDouble();
+            double cumulative = 0.0;
+            int selectedClassIndex = 0;
+            for (int i = 0; i < classOddsDict.Count; i++)
+            {
+                cumulative += Convert.ToDouble(classOddsDict[i]);
+                if (randomColorRoll < cumulative)
+                {
+                    selectedClassIndex = i;
+                    break;
+                }
+            }
+            string selectedColor = classOddsXml.ChildNodes[selectedClassIndex].SelectNodes("classColor")[0].InnerText;
+
+            // Creates list of possible guns based off class color, stores in XmlNodeList `classGunsArr`
+            XmlNodeList classGunsArr = selectedCase["possibleGuns"].SelectNodes("gun[gunClass='" + selectedColor + "']");
+
+            // Selects random gun, stores in XmlNode `selectedGun`
+            int randomGunRoll = randomObject.Next(0, classGunsArr.Count);
+            XmlNode selectedGun = classGunsArr[randomGunRoll];
+
+            // Display roll output
+            Color selectedColorVar = Color.Green;
+            switch (selectedClassIndex) {
+                case 0:
+                    selectedColorVar = Color.Blue;
+                    break;
+                case 1:
+                    selectedColorVar = Color.FromArgb(92, 61, 153);
+                    break;
+                case 2:
+                    selectedColorVar = Color.FromArgb(204, 51, 153);
+                    break;
+                case 3:
+                    selectedColorVar = Color.Red;
+                    break;
+                case 4:
+                    selectedColorVar = Color.Gold;
+                    break;
+            }
+            rollOutputRichTextBox.ForeColor = selectedColorVar;
+            rollOutputRichTextBox.Text = (selectedGun["gunName"].InnerText + " | " + selectedGun["gunSkin"].InnerText);
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (caseListBox.SelectedItem != null)
+            {
+                string selectedCaseName = caseListBox.SelectedItem.ToString();
+                rollOutputRichTextBox.Text = ("Case selected: " + selectedCaseName);
+            }
+        }
+    }
+}
